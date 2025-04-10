bd6dafa07a82a483c692e3c1953e0737bfb41692
Damian Amaris <damian.bolatti@amaris.com>
2025-04-10 14:58:28 +0200

    tailwind setup

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,11 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import "./global.css"
 
 const App = () => {
   return (
-    <View>
-      <Text>App</Text>
+    <View className='mt-10 ms-10'>
+      <Text className='text-3xl font-bold'>Travel App</Text>
     </View>
   )
 }
