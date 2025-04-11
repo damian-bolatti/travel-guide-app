9f006b8c3b17c0dd01e4d0f70e650ebe94da1860
Damian Amaris <damian.bolatti@amaris.com>
2025-04-11 16:23:03 +0200

    Update android environment

--- core/infrastructure/graphql/fetchGraphQL.ts
+++ core/infrastructure/graphql/fetchGraphQL.ts
@@ -1,8 +1,13 @@
+import { Platform } from "react-native";
+
 export const fetchGraphQL = async (
     query: string,
     variables: Record<string, any> = {}
   ): Promise<any> => {
-    const GRAPHQL_ENDPOINT = process.env.EXPO_PUBLIC_GRAPHQL_ENDPOINT as string;
+    const GRAPHQL_ENDPOINT =
+  Platform.OS === 'web'
+    ? process.env.EXPO_PUBLIC_GRAPHQL_ENDPOINT_WEB
+    : process.env.EXPO_PUBLIC_GRAPHQL_ENDPOINT_ANDROID;
   
     if (!GRAPHQL_ENDPOINT) {
       throw new Error('GRAPHQL_ENDPOINT is not defined in the environment variables');
