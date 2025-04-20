b9ca429a0662875c6330b45f2a06ce728fe04411
damian-bolatti <damianbolatti@gmail.com>
2025-04-20 17:16:44 +0200

    Update envs

--- core/infrastructure/graphql/config/graphqlConfig.ts
+++ core/infrastructure/graphql/config/graphqlConfig.ts
@@ -1,15 +1,9 @@
 import { Platform } from 'react-native';
-import Constants from 'expo-constants';
 
-const {
-  EXPO_PUBLIC_GRAPHQL_ENDPOINT_WEB: graphqlEndpointWeb,
-  EXPO_PUBLIC_GRAPHQL_ENDPOINT_ANDROID: graphqlEndpointAndroid,
-  EXPO_PUBLIC_GRAPHQL_TIMEOUT: graphqlTimeout,
-} = Constants.manifest?.extra || {
-  EXPO_PUBLIC_GRAPHQL_ENDPOINT_WEB: "http://localhost:4000/graphql",
-  EXPO_PUBLIC_GRAPHQL_ENDPOINT_ANDROID: "http://10.0.2.2:4000/graphql",
-  EXPO_PUBLIC_GRAPHQL_TIMEOUT: 5000,
-};
+  // Use .env params
+  const graphqlEndpointWeb = "http://localhost:4000/graphql"
+  const graphqlEndpointAndroid = "http://10.0.2.2:4000/graphql"
+  const graphqlTimeout = 5000
 
 export const GRAPHQL_ENDPOINT =
   Platform.OS === 'web' ? graphqlEndpointWeb : graphqlEndpointAndroid;
